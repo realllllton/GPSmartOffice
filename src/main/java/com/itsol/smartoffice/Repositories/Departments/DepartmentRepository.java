327ef131be55e7899a6d613e96f4a316be163929
Your Name <you@example.com>
2019-09-20 15:46:03 +0700

    update 3h35 20/09/2019

--- src/main/java/com/itsol/smartoffice/Repositories/Departments/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.itsol.smartoffice.Repositories.Departments;
-
-import com.itsol.smartoffice.dto.DepartmentDto;
-
-import java.util.List;
-
-public interface DepartmentRepository {
-    List<DepartmentDto> getdepartall();
-}
--- src/main/java/com/itsol/smartoffice/Services/Departments/DepartmentService.java
+++
@@ -1,9 +0,0 @@
-package com.itsol.smartoffice.Services.Departments;
-
-import com.itsol.smartoffice.dto.DepartmentDto;
-
-import java.util.List;
-
-public interface DepartmentService {
-List<DepartmentDto> getdepartall();
-}
